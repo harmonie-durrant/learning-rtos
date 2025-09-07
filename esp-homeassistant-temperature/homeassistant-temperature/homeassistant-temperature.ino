eff283c1573f1e40dde24bd37510c11f6369e66c
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-09-07 12:38:43 +0000

    Add homeassistant-temperature - Add base to start from TODO: Add home assistant integration after homeassistant-led is done

---
+++ esp-homeassistant-temperature/homeassistant-temperature/homeassistant-temperature.ino
@@ -0,0 +1,44 @@
+#include <WiFi.h>
+#include "DHT.h"
+
+// Pins
+#define DHTPIN 13
+#define DHTTYPE DHT11
+static const int led_pin = 2;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid     = "Harmonie";
+const char* password = "password";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  dht.begin();
+}
+
+void loop() {
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+  delay(2000);
+}
